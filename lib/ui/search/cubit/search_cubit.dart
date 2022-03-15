e4c0e2b1ea6f2f6ac07a478fe1374811a5899e2c
mserif <mehmetserif@gmail.com>
2022-03-15 14:22:48 +0300

    Clean arch neredeyse bitti, di eklendi. Search cubit yapısı eklendi.

--- lib/ui/search/cubit/search_cubit.dart
+++ lib/ui/search/cubit/search_cubit.dart
@@ -1,8 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../domain/entities/food.dart';
+import '../../../domain/usecases/food/get_food_on_name_usecase.dart';
+import '../../../domain/usecases/food/params/get_food_param.dart';
+
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit() : super(SearchInitial());
+  final GetFoodOnNameUseCase? searchUseCase;
+  SearchCubit({
+    this.searchUseCase,
+  }) : super(SearchInitial());
+
+  late Food _food;
+  Future<void> getSearchItem(String foodName) async {
+    emit(Searching());
+    final response = await searchUseCase?.call(GetFoodParam(foodName));
+
+    if (response != null) {
+      _food = response;
+      emit(SearchSuccess(_food));
+    } else {
+      emit(const SearchFailure("No food found"));
+    }
+  }
 }
