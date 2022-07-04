be3ebd3ea3a5777e6382f6d42b1767d7f8a716b0
mserif <mehmetserif@gmail.com>
2022-07-04 15:52:55 +0300

    Bolus hesaplama formülleri eklendi, sonuç için dialogbox eklendi.

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -3,4 +3,20 @@ import 'package:flutter/material.dart';
 class Utils {
   static List<Widget> modelBuilder<M>(List<M> models, Widget Function(int index, M model) builder) =>
       models.asMap().map<int, Widget>((index, model) => MapEntry(index, builder(index, model))).values.toList();
+
+  static animateDialogGrow(Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    return ScaleTransition(
+      scale: Tween<double>(end: 1.0, begin: 0.0).animate(
+        CurvedAnimation(
+          parent: animation,
+          curve: const Interval(
+            0.00,
+            0.50,
+            curve: Curves.linear,
+          ),
+        ),
+      ),
+      child: child,
+    );
+  }
 }
